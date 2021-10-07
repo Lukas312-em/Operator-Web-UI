817a128e0b20077022652a0328688fc702c9e10f
Lukas312-em <lukas.schaefer@psiori.com>
2021-10-07 16:37:11 +0200

    setup firebase

--- operator-web/src/app/app.module.ts
+++ operator-web/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyBpA685rxaiBY1ixqr7wVBjZoKYoBR93xI",
+      authDomain: "testing-fire-6a0e7.firebaseapp.com",
+      projectId: "testing-fire-6a0e7",
+      storageBucket: "testing-fire-6a0e7.appspot.com",
+      messagingSenderId: "36419784526",
+      appId: "1:36419784526:web:fb0ac76cffdfed67b0181f",
+      measurementId: "G-NB8K1VTC2H"
+    })),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
